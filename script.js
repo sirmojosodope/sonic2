86d92a07504db09f2a0b806cce5949fa57f8c7fd
sirmojosodope <stoverlee29@gmail.com>
2022-03-23 05:32:24 -0400

    Fixed bottom button

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-console.log("Your external JavaScript file is linked 🎉")
+console.log("connected")
+
+var sendBtn = document.getElementById("sendBtn");
+var firstName = document.getElementById("firstName");
+var lastName = document.getElementById("lastName");
+var textBox = document.getElementById("textArea");
+var genderSelect = document.getElementById("genderSelect");
+var age = document.getElementById("age");
+var bDay = document.getElementById("bDay");
+
+var userDiv = document.getElementById("userInfo")
+var nameDiv = document.getElementById("name");
+var attributes = document.getElementById("attributes");
+
+sendBtn.addEventListener("click", function(){
+
+    var userContact = {
+        firstName: firstName.value,
+        lastName: lastName.value,
+        message: textBox.value,
+        gender: genderSelect.value,
+        age: age.value,
+        birthday: bDay.value
+    }
+
+    nameDiv.append(userContact.firstName + " " + userContact.lastName)
+    attributes.append(userContact.gender, userContact.age, userContact.birthday)
+    userDiv.append(nameDiv, attributes);
+
+
+})
